35870049f3c6f1534767690ac337b9ec61725a76
Dmitry R <dimsog@gmail.com>
2022-07-21 22:33:55 +0300

    Добавляю сервис подсчета количества пользователей в группе

--- app/Providers/VkServiceProvider.php
+++ app/Providers/VkServiceProvider.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Services\Vk\UsersCountByGroup;
 use App\Services\Vk\Utils\GenerateVkAccessTokenLink;
 use Illuminate\Support\ServiceProvider;
+use VK\Client\VKApiClient;
 use VK\OAuth\VKOAuth;
 
 class VkServiceProvider extends ServiceProvider
@@ -14,6 +16,13 @@ class VkServiceProvider extends ServiceProvider
     {
         $this->app->bind(GenerateVkAccessTokenLink::class, function () {
             return new GenerateVkAccessTokenLink(config('services.vk.client_id'), new VKOAuth());
+        });
+
+        $this->app->bind(UsersCountByGroup::class, function () {
+            return new UsersCountByGroup(
+                config('services.vk.access_token'),
+                $this->app->make(VKApiClient::class)
+            );
         });
     }
 }
