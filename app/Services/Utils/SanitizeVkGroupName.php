b403bac5dda497272f1c69828e7f35992e88fcf3
Dmitry R <dimsog@gmail.com>
2022-07-21 19:10:13 +0300

    Extract a group name from link

---
+++ app/Services/Utils/SanitizeVkGroupName.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Utils;
+
+class SanitizeVkGroupName
+{
+    public static function sanitize(string $groupLink): string
+    {
+        $groupLink = explode('?', trim($groupLink))[0];
+        return str_replace([
+            'https://vk.com/',
+            'https://m.vk.com/',
+            'http://vk.com/',
+            'http://m.vk.com/',
+            '/',
+        ], '', $groupLink);
+    }
+}
---
+++ tests/Unit/App/Services/Utils/SanitizeVkGroupNameTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\App\Services\Utils;
+
+use App\Services\Utils\SanitizeVkGroupName;
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testSanitize()
+    {
+        $this->assertEquals('demo', SanitizeVkGroupName::sanitize('https://vk.com/demo'));
+        $this->assertEquals('demo', SanitizeVkGroupName::sanitize('http://vk.com/demo'));
+        $this->assertEquals('demo', SanitizeVkGroupName::sanitize('https://m.vk.com/demo'));
+        $this->assertEquals('demo', SanitizeVkGroupName::sanitize('http://m.vk.com/demo'));
+        $this->assertEquals('demo', SanitizeVkGroupName::sanitize('http://m.vk.com/demo/?test=1'));
+    }
+}
