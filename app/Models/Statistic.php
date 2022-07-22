568e2dae9540db41896ea942a333cfcda8275bc7
Dmitry R <dimsog@gmail.com>
2022-07-22 15:17:55 +0300

    Добавил ide-helper

--- app/Models/Statistic.php
+++ app/Models/Statistic.php
@@ -7,6 +7,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Statistic
+ *
+ * @property int $id
+ * @property string $date
+ * @property int $group_id
+ * @property int $user_id
+ * @property int $type
+ * @property int $initial
+ * @method static \Illuminate\Database\Eloquent\Builder|Statistic newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Statistic newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Statistic query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Statistic whereDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Statistic whereGroupId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Statistic whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Statistic whereInitial($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Statistic whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Statistic whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Statistic extends Model
 {
     use HasFactory;
