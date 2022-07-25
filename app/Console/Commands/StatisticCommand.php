a01688b09084c04bd78f5e34b459e97eaddcbeac
Dmitry R <dimsog@gmail.com>
2022-07-25 22:52:15 +0300

    Рефакторинг: подставляю ID группы из бд

--- app/Console/Commands/StatisticCommand.php
+++ app/Console/Commands/StatisticCommand.php
@@ -4,21 +4,24 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
+use App\Models\Group;
 use App\Services\Statistics\DiffGroupUsersHandler;
 use App\Services\Vk\GroupInfoFetcher;
 use Illuminate\Console\Command;
 
 class StatisticCommand extends Command
 {
-    protected $signature = 'vk:demo {groupId}';
+    protected $signature = 'vk:demo {vkGroupId}';
 
     protected $description = 'Демо-команда для управления статистикой';
 
 
     public function handle(DiffGroupUsersHandler $handler, GroupInfoFetcher $groupInfoFetcher): int
     {
-        $groupId = $this->argument('groupId');
-        $handler->handle($groupInfoFetcher->getGroupInfoById($groupId));
+        $vkGroupId = $this->argument('vkGroupId');
+        $vkGroup = $groupInfoFetcher->getGroupInfoById($vkGroupId);
+        $group = Group::findByVkGroupIdOrFail($vkGroup->id);
+        $handler->handle($group->id);
         return 0;
     }
 }
