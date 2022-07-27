7605e10e5bc343a06d5fc7f5d39715a41725da05
Dmitry R <dimsog@gmail.com>
2022-07-27 18:04:23 +0300

    Подключил css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <title>{{ $title }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite('resources/css/app.scss')
 </head>
 <body>
     {{ $slot  }}
