a709be174f0ffd2a6e2c54db518d2292f95654b6
Dmitry R <dimsog@gmail.com>
2022-07-27 18:21:07 +0300

    Добавляю удаление vk.com и m.vk.com

--- resources/js/helpers/VkGroupIdExtractor.js
+++ resources/js/helpers/VkGroupIdExtractor.js
@@ -2,7 +2,7 @@ export default class {
     static extract(groupLink) {
         return groupLink
             .split('?')[0]
-            .replace(/https\:\/\/vk\.com\/|https\:\/\/m\.vk\.com\/|http\:\/\/vk\.com\/|http\:\/\/m\.vk\.com\//, '')
+            .replace(/https\:\/\/vk\.com\/|https\:\/\/m\.vk\.com\/|http\:\/\/vk\.com\/|http\:\/\/m\.vk\.com\/|vk\.com\/|m\.vk\.com\/|\//, '')
             .trim();
     }
 }
