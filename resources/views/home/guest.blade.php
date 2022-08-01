8dc8c784c9fb59e398f994eeb9e125b49a5c874e
Dmitry R <dimsog@gmail.com>
2022-08-01 18:28:20 +0300

    Базовая верстка страницы гостя

---
+++ resources/views/home/guest.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:title>Мониторинг групп ВК</x-slot:title>
+    <div class="guest-block">
+        <h1>Войдите, чтобы начать пользоваться системой</h1>
+        <a href="{{ route('auth.redirect') }}">Войти</a>
+    </div>
+</x-layout>
