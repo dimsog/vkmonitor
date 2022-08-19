34a7ca428805717fe535633b970588c1956619af
Dmitry R <dimsog@gmail.com>
2022-08-19 14:08:24 +0300

    feat: вывел список групп

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -2,26 +2,40 @@
     <div>
         <button class="btn btn-vk" @click.prevent="onShowAddGroupModal">Добавить группу</button>
         <v-group-editor ref="groupEditor"></v-group-editor>
+
+        <v-groups :groups="groups"></v-groups>
     </div>
 </template>
 
 <script>
 import VGroupEditor from "./VGroupEditor.vue";
+import VGroups from "./VGroups.vue";
+import GroupsService from "../api/GroupsService";
 
 export default {
     components: {
-        VGroupEditor
+        VGroupEditor,
+        VGroups
     },
 
     data() {
         return {
             vkGroupId: null,
+            groups: []
         }
+    },
+
+    created() {
+        this.fetchGroups();
     },
 
     methods: {
         onShowAddGroupModal() {
             this.$refs.groupEditor.show();
+        },
+
+        async fetchGroups() {
+            this.groups = await GroupsService.fetchGroups();
         }
     }
 }
---
+++ resources/js/components/VGroups.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <a v-for="group in groups" href="#">
+            {{ group.vkGroup.name }}
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        groups: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
