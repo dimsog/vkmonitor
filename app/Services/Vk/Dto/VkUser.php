8ab5de03f2034cf59cc176ea1841c59caa648654
Dmitry R <dimsog@gmail.com>
2022-08-20 13:59:48 +0300

    feat: добавил класс VkUser

---
+++ app/Services/Vk/Dto/VkUser.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Vk\Dto;
+
+class VkUser
+{
+    public function __construct(
+        public readonly int $id,
+        public readonly string $firstName,
+        public readonly string $lastName,
+        public readonly string $screenName,
+        public readonly string $photo200
+    )
+    {}
+}
