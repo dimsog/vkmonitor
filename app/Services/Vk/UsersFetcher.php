8ec38f3cb05bb403cf824ab2ea011648a22b45a2
Dmitry R <dimsog@gmail.com>
2022-08-20 14:41:50 +0300

    feat: добавил класс UsersFetcher

---
+++ app/Services/Vk/UsersFetcher.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Vk;
+
+use App\Services\Vk\Dto\VkUser;
+use Illuminate\Support\Collection;
+use VK\Client\VKApiClient;
+
+class UsersFetcher
+{
+    public function __construct(
+        private readonly VKApiClient $vkApiClient
+    )
+    {}
+
+    /**
+     * @param Collection $vkUserIds
+     * @param string $accessToken
+     * @return Collection<VkUser>
+     */
+    public function fetchAllByIds(Collection $vkUserIds, string $accessToken): Collection
+    {
+        $result = [];
+        foreach ($vkUserIds->chunk(500) as $users) {
+            $response = $this->vkApiClient->users()->get($accessToken, [
+                'user_ids' => $users->toArray(),
+                'fields' => 'photo_200,screen_name'
+            ]);
+            foreach ($response as $rawUser) {
+                $result[] = new VkUser(
+                    $rawUser['id'],
+                    $rawUser['first_name'],
+                    $rawUser['last_name'],
+                    $rawUser['screen_name'],
+                    $rawUser['photo_200'],
+                );
+            }
+        }
+        return collect($result);
+    }
+}
