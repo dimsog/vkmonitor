8d375ab1ef245fb31495ced5208506f701094b10
Dmitry R <dimsog@gmail.com>
2022-08-21 23:37:39 +0300

    feat(tmp): начало работы над выводом пользователей

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -3,25 +3,29 @@
         <button class="btn btn-vk" @click.prevent="onShowAddGroupModal">Добавить группу</button>
         <v-group-editor ref="groupEditor"></v-group-editor>
 
-        <v-groups :groups="groups"></v-groups>
+        <v-groups :groups="groups" @select="onSelectGroup"></v-groups>
+        <v-diff-users v-if="activeGroup != null" :group="activeGroup"></v-diff-users>
     </div>
 </template>
 
 <script>
 import VGroupEditor from "./VGroupEditor.vue";
 import VGroups from "./VGroups.vue";
+import VDiffUsers from "./VDiffUsers.vue";
 import GroupsService from "../api/GroupsService";
 
 export default {
     components: {
         VGroupEditor,
-        VGroups
+        VGroups,
+        VDiffUsers
     },
 
     data() {
         return {
             vkGroupId: null,
-            groups: []
+            groups: [],
+            activeGroup: null
         }
     },
 
@@ -36,6 +40,10 @@ export default {
 
         async fetchGroups() {
             this.groups = await GroupsService.fetchGroups();
+        },
+
+        onSelectGroup(group) {
+            this.activeGroup = group;
         }
     }
 }
---
+++ resources/js/components/VDiffUsers.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        {{ group.vkGroup.name }}
+
+        <div>
+            <div v-for="diff in diffItems">
+                <div>{{ diff.weekName }}, {{ diff.date }}</div>
+                <div v-for="user in diff.users">
+                    {{ user.vkUser.firstName }} {{ user.vkUser.lastName }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import DiffUsersReaderService from "../api/DiffUsersReaderService";
+
+export default {
+    props: {
+        group: {
+            type: Object,
+            required: true
+        }
+    },
+
+    data() {
+        return {
+            diffItems: []
+        }
+    },
+
+    mounted() {
+        this.fetchDiffItems();
+    },
+
+    methods: {
+        async fetchDiffItems() {
+            this.diffItems = await DiffUsersReaderService.fetch(this.group.id);
+        }
+    },
+
+    watch: {
+        group() {
+            this.fetchDiffItems();
+        }
+    }
+}
+</script>
