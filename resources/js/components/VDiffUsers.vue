4595f86c936caa87c0376d85cad92f006abd90a4
Dmitry R <dimsog@gmail.com>
2022-08-23 00:20:42 +0300

    feat: стили для списка людей

--- resources/js/components/VDiffUsers.vue
+++ resources/js/components/VDiffUsers.vue
@@ -1,17 +1,65 @@
 <template>
-    <div>
+    <div class="diff-items">
         <h1>{{ group.vkGroup.name }}</h1>
 
         <div>
-            <div v-for="diff in diffItems">
-                <div>{{ diff.weekName }}, {{ diff.date }}</div>
-                <div v-for="user in diff.users">
-                    {{ user.vkUser.firstName }} {{ user.vkUser.lastName }}
+            <div v-for="diff in diffItems" class="diff-item">
+                <div class="diff-item__date">
+                    <strong>{{ diff.weekName }}, {{ diff.date }}</strong>
+                </div>
+                <div class="diff-item__users">
+                    <a v-for="user in diff.users"
+                       class="diff-item-user"
+                       :class="{'diff-item-user--subscribed': user.subscribed}"
+                       :href="'https://vk.com/' + user.vkUser.screenName"
+                       target="_blank"
+                    >
+                        <div class="diff-item-user__avatar">
+                            <img :src="user.vkUser.photo200">
+                        </div>
+                        <div class="diff-item-user__name">
+                            {{ user.vkUser.firstName }}<br> {{ user.vkUser.lastName }}
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.diff-items {
+    .diff-item {
+        .diff-item__users {
+            display: flex;
+            .diff-item-user {
+                display: block;
+                width: 80px;
+                text-align: center;
+                color: #666;
+                text-decoration: none;
+                margin: 0 8px 8px 0;
+                .diff-item-user__avatar {
+                    img {
+                        max-width: 100%;
+                        max-height: 100%;
+                        border-radius: 50%;
+                        border: 3px solid #ffa0a0;
+                    }
+                }
+                .diff-item-user__name {
+                    font-size: .8rem;
+                }
+                &.diff-item-user--subscribed {
+                    img {
+                        border-color: #9be9a8;
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
 
 <script>
 import DiffUsersReaderService from "../api/DiffUsersReaderService";
