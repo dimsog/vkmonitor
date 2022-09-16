aa75880b707bf6d39ec2762d275e4dabc681e7fa
Dmitry R <dimsog@gmail.com>
2022-09-16 15:09:17 +0300

    feat: кнопка с пользователем

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -3,14 +3,19 @@
         <div class="content">
             <div class="sidebar">
                 <div class="sidebar__toolbar">
-                    <button class="btn btn-vk" @click.prevent="onShowAddGroupModal">Добавить группу</button>
-                    <a v-if="isShowSettings" @click.prevent="onShowSettings" href="#" class="btn btn-outline-secondary">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"></path>
-                            <circle cx="12" cy="12" r="3"></circle>
-                        </svg>
-                    </a>
+                    <div>
+                        <button class="btn btn-vk" @click.prevent="onShowAddGroupModal">Добавить группу</button>
+                    </div>
+                    <div class="d-flex">
+                        <v-profile v-if="user != null" :user="user"></v-profile>
+                        <a v-if="isShowSettings" @click.prevent="onShowSettings" href="#" class="btn btn-outline-secondary">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"></path>
+                                <circle cx="12" cy="12" r="3"></circle>
+                            </svg>
+                        </a>
+                    </div>
                 </div>
                 <v-groups :groups="groups" @select="onSelectGroup"></v-groups>
             </div>
@@ -60,6 +65,7 @@ import VGroupEditor from "./VGroupEditor.vue";
 import VGroups from "./VGroups.vue";
 import VDiffUsers from "./VDiffUsers.vue";
 import VSettings from "./VSettings.vue";
+import VProfile from "./VProfile.vue";
 import GroupsService from "../api/GroupsService";
 import UserService from "../api/UserService";
 
@@ -68,7 +74,8 @@ export default {
         VGroupEditor,
         VGroups,
         VDiffUsers,
-        VSettings
+        VSettings,
+        VProfile
     },
 
     data() {
---
+++ resources/js/components/VProfile.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="dropdown user-profile">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+            <img :src="user.avatar" class="user-profile__image" alt="">
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li>
+                <a @click.prevent="onLogout()" class="dropdown-item" href="#">Выйти</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.user-profile {
+    button {
+        background: transparent;
+        border: none;
+        outline: none !important;
+        box-shadow: none !important;
+        &::after {
+            display: none;
+        }
+        .user-profile__image {
+            width: 32px;
+            height: 32px;
+            border-radius: 50%;
+        }
+    }
+}
+</style>
+
+<script>
+import UserService from "../api/UserService";
+
+export default {
+    props: {
+        user: {
+            type: Object,
+            required: true
+        }
+    },
+
+    methods: {
+        async onLogout() {
+            await UserService.logout();
+            location.reload();
+        }
+    }
+}
+</script>
