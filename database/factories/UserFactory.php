e68f820dd2ac7d9d45e75b328ea11c3c4017360f
Dmitry R <dimsog@gmail.com>
2022-10-07 20:25:22 +0300

    refactoring: меняю поля для фабрики пользователей

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,28 +15,13 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition()
+    public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'vk_id' => mt_rand(1, 100000),
+            'name' => 'Test test',
+            'avatar' => '',
             'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
     }
 }
