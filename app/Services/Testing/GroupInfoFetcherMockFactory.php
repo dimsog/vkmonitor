a658b758087079c151b536e0d05915fc9d798b12
Dmitry R <dimsog@gmail.com>
2022-10-09 12:11:23 +0300

    feat: добавил getGroupInfoByIds

--- app/Services/Testing/GroupInfoFetcherMockFactory.php
+++ app/Services/Testing/GroupInfoFetcherMockFactory.php
@@ -14,11 +14,21 @@ class GroupInfoFetcherMockFactory
         return Mockery::mock(GroupInfoFetcher::class, static function (MockInterface $mock) use ($userIds): void {
             $mock
                 ->shouldReceive('getGroupInfoById')
-                ->andReturn(new VkGroup(1, 'ApiClub', count($userIds), 'apiclub', '/path/to/photo'));
+                ->andReturn(static::getVkGroup($userIds));
 
             $mock
                 ->shouldReceive('users')
                 ->andReturn($userIds);
+
+            $mock->shouldReceive('getGroupInfoByIds')
+                ->andReturn(collect([
+                    static::getVkGroup([])
+                ]));
         });
+    }
+
+    private static function getVkGroup(array $userIds): VkGroup
+    {
+        return new VkGroup(1, 'ApiClub', count($userIds), 'apiclub', '/path/to/photo');
     }
 }
