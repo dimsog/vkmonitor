7910698b299a007390b371439968976d943aebcf
Dmitry R <dimsog@gmail.com>
2022-10-10 23:38:21 +0300

    feat: делаю проверку, что tokens передан

--- app/Services/Settings/SettingsStore.php
+++ app/Services/Settings/SettingsStore.php
@@ -2,12 +2,17 @@
 
 namespace App\Services\Settings;
 
+use InvalidArgumentException;
 use App\Models\VkToken;
+use Illuminate\Support\Arr;
 
 class SettingsStore
 {
     public function store(array $settings)
     {
+        if (!Arr::has($settings, 'tokens')) {
+            throw new InvalidArgumentException('Не передан параметр tokens');
+        }
         // 1) удаляем токены, которых уже нет
         $tokenIds = array_column($settings['tokens'], 'id');
         VkToken::deleteOldTokens($tokenIds);
